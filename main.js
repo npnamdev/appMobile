dc2133d7c946f2b0fbdc5c6fd12225caf19436a6
Nguyễn Phương Nam <npnam031101@gmail.com>
2023-06-08 16:51:21 +0700

    test

--- main.js
+++ main.js
@@ -9,11 +9,14 @@ btn.addEventListener('click', () => {
 const icon_menu = document.querySelector('.icon-menu');
 const icon_close = document.querySelector('.icon-close');
 const sidebar = document.querySelector('#sidebar');
+const overline = document.querySelector('.overline');
 
 icon_menu.addEventListener('click', () => {
     sidebar.classList.add('active');
+    overline.classList.add('active');
 })
 
 icon_close.addEventListener('click', () => {
     sidebar.classList.remove('active');
+    overline.classList.remove('active');
 })
